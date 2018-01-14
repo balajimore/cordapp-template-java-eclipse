5d4c94477bfe2ef114f4839cbc0fe469674a5a92
balajimore <more.balajib@gmail.com>
2018-01-14 18:38:05 +0530

    CorDapp java template for eclipse ide.

---
+++ cordapp-contracts-states/src/main/java/com/template/TemplateState.java
@@ -0,0 +1,15 @@
+package com.template;
+
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Define your state object here.
+ */
+public class TemplateState implements ContractState {
+    /** The public keys of the involved parties. */
+    @Override public List<AbstractParty> getParticipants() { return Collections.emptyList(); }
+}
